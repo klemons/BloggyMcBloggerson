d9feca6ada45477e58e0a2cd46ffa83cc8f2fb4c
JosephSwedlund <33673774+JosephSwedlund@users.noreply.github.com>
2019-03-18 14:03:05 -0500

    NEWEST VERSION

--- write.php
+++ write.php
@@ -1,7 +1,12 @@
-<!doctype html>
+<!DOCTYPE html>
+<?php
+	session_start();
+	if(!(isset($_SESSION['logged_in'])) || $_SESSION['logged_in'] == false)
+    header("Location: index.php")
+?>
 
+<html lang="en">
 <head>
-	 <head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -11,10 +16,46 @@
     <title>Blog</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/sign-in/">
-</head>
+
+    <!-- Bootstrap core CSS -->
+  </head>
+
 <body style="background-color:#E9E9E9">>
 
-	<header>
+
+  <?php
+
+if(isset($_POST['postTitle'])  && isset($_POST['postContent']))
+{
+  $blogStory = htmlentities($_POST['postContent']);
+  $storyTitle = htmlentities($_POST['postTitle']);
+
+  $servername = "localhost";
+  $username = "rootKing";
+  $password = "C0xPMVgeEKELbN1e";
+  $dbname = "WebAppDB";
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  } 
+
+  $authorsID = $_SESSION["id"];
+  $authorsUsername = $_SESSION["username"];
+  // Insert values
+  $sql = "INSERT INTO postData (title, story, authorId, authorName, isPosted)
+  VALUES ( '$storyTitle', '$blogStory', '$authorsID', '$authorsUsername', 0 )";
+  if ($conn->query($sql) === TRUE) {
+  } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  $conn->close();
+}
+?>
+
+
+<header>
       <!-- Fixed navbar -->
       <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
         <a class="navbar-brand" href="main.php">BloggyMcBloggerson</a>
@@ -35,43 +76,20 @@
     </header>
 
 
-<?php
-    if(isset($_POST['read']))
-    {
-      $servername = "localhost";
-      $username = "rootKing";
-      $password = "C0xPMVgeEKELbN1e";
-      $dbname = "WebAppDB";
-      // Create connection
-      $conn = new mysqli($servername, $username, $password, $dbname);
+<div class="container" style="padding-top: 75px">
+	<form method="post">
+		<div class="form-group">
+    		<input type="text" name="postTitle" form-control" placeholder="Title"required autofocus>
+        <textarea name="postContent" class="form-control" rows="20" id="comment" placeholder="Body"></textarea>
+			<button class="btn btn-outline-success my-2 my-sm-0 btn-block" type="submit">Submit For Review</button>
 
-      // Check connection
-      if ($conn->connect_error) {
-          die("Connection failed: " . $conn->connect_error);
-          echo "3";
-      } 
+   		</div>
+   </form>
+</div>
 
-	 $sql = "SELECT * FROM postData WHERE postId = ".$_POST['read'];
-      $result = $conn->query($sql);
-
-
-		//have DB info.
-  	if ($result->num_rows > 0) {
-    	while ($row = $result->fetch_assoc()) {
-    	echo "<br/>";
-    	echo "<br/>"; 
-    	echo "<br/>";
-    	echo "<br/>";
-    	echo "<br/>";
-
-      echo '<main role="main" class="container"> <h1 class="mt-5">'; 
-      echo $row["title"]; 
-      echo'</h1> <p class="lead">'; 
-      echo $row["story"]; 
-      echo'</p> </main>';
-		}
-      $conn->close();
-		}
-	}
-?>
 </body>
+</html>
+
+</form>
+
+</div>
